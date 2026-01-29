639ad5877a5b9087f887a5b1fb9c5f5c9617a366
narasimhanarsi666-ship-it <narasimhanarsi666@gmail.com>
2026-01-29 16:48:52 +0530

    Update Cart.py
    change Cart

--- frontend/src/pages/Cart.py
+++ frontend/src/pages/Cart.py
@@ -1,4 +1,5 @@
 from services.cartService import add_item,get_cart
 def add_to_cart_flow(i): return add_item(i)
 def view_cart_flow(): return get_cart()
-  ####
+def view_cart_flows(): return get_cart()
+
